4960bab5f808fc9907bbe0dab996dd217d9d4ca7
Omwomo <eugeneoduor79@gmail.com>
2023-11-03 13:42:46 +0300

    Solved the fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,6 +1,17 @@
 class Solver
   def factorial; end
-  def fizzbuzz; end
+  def fizzbuzz(n)
+    case
+    when n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    when n % 3 == 0
+      'fizz'
+    when n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
 
 def reverse(word)
