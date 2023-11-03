cba2ce3a848dc4586cd2c8fe1eb318875e924fa9
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-03 11:34:50 +0100

    add reverse method

--- solver.rb
+++ solver.rb
@@ -2,3 +2,7 @@ class Solver
   def factorial; end
   def fizzbuzz; end
 end
+
+def reverse(word)
+  word.reverse
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -41,3 +41,11 @@ describe Solver do
     end
   end
 end
+
+describe '#reverse' do
+    it 'returns the reversed word' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+  end
+  
